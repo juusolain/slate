afe76e00c06b74e3c9b242727114b7dfacae142a
jusola <juulain@gmail.com>
2021-05-04 16:34:46 +0300

    Add tailwind to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,5 +7,24 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\,css&/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss',
+            plugins: [
+              require('tailwindcss'),
+              require('autoprefixer')
+            ]
+          }
+        }
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
+    return config
+  }
 }
