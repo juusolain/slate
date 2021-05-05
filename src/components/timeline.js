ec53ff922318a8c8ca8ed8316819e5d5152dede7
jusola <juulain@gmail.com>
2021-05-05 09:36:48 +0300

    Improve timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,23 +1,31 @@
-import React, {useState} from "react";
-import TimelineEvent from './timeline/event'
+import React, { useState } from "react";
+import TimelineEvent from "./timeline/event";
 
 export default function Timeline({ events, ...props }) {
-    const [timelineWidth, setTimelineWidth] = useState(500)
-    const [timelineStart, setTimelineStart] = useState(0) 
+  const [timelineWidth, setTimelineWidth] = useState(500);
+  const [timelineStart, setTimelineStart] = useState(0);
 
-    const eventComps = events.map((ev, i) => 
-        <TimelineEvent event={ev} key={i} timelineWidth={timelineWidth} timelineStart={timelineStart}/>
-    )
-
-    const onWheelHandler = (e) => {
-        setTimelineWidth(timelineWidth + e.deltaY)
-    }
-
+  const eventComps = events.map((ev, i) => {
     return (
-        <div className="bg-gray-100 flex-1 h-24 overflow-hidden" onWheel={onWheelHandler}>
-            <div className="relative h-24">
-                {eventComps}
-            </div>
-        </div>
+      <TimelineEvent
+        event={ev}
+        key={i}
+        timelineWidth={timelineWidth}
+        timelineStart={timelineStart}
+      />
     );
+  });
+
+  const onWheelHandler = (e) => {
+    setTimelineWidth(timelineWidth + (e.deltaY * timelineWidth) / 100);
+  };
+
+  return (
+    <div
+      className="bg-gray-100 flex-1 h-24 overflow-hidden"
+      onWheel={onWheelHandler}
+    >
+      <div className="relative h-24">{eventComps}</div>
+    </div>
+  );
 }
--- src/components/timeline/event.js
+++ src/components/timeline/event.js
@@ -1,28 +1,47 @@
 import React from "react";
 
-export default function TimelineEvent({ event: { name, color, description, duration, startTime }, timelineWidth, timelineStart }) {
-  return (
-    <div className="absolute float-left h-full" style={{
-      width: getWidth(duration, timelineWidth),
-      left: getLeft(startTime, timelineWidth, timelineStart)
-    }}>
+export default function TimelineEvent({
+  event: { name, color, description, duration, startTime },
+  timelineWidth,
+  timelineStart,
+}) {
+  if (shoudBeVisible(startTime, duration, timelineStart, timelineWidth)) {
+    return (
       <div
-        className="p-2 h-full border rounded border-transparent whitespace-nowrap overflow-hidden overflow-ellipsis" style={{
-          backgroundColor: color
+        className="absolute float-left h-full"
+        style={{
+          width: getWidth(duration, timelineWidth),
+          left: getLeft(startTime, timelineWidth, timelineStart),
         }}
       >
-        <p className="text-white text-lg m-0">{name}</p>
-        <p className="text-gray-400 m-0">{description}</p>
+        <div
+          className="p-2 h-full border rounded border-transparent whitespace-nowrap overflow-hidden overflow-ellipsis"
+          style={{
+            backgroundColor: color,
+          }}
+        >
+          <p className="text-white text-lg m-0">{name}</p>
+          <p className="text-gray-400 m-0">{description}</p>
+        </div>
       </div>
-    </div>
+    );
+  }
+  return null;
+}
 
+function getWidth(duration, timelineWidth) {
+  return (duration / timelineWidth) * 100 + "%";
+}
+
+function getLeft(startTime, timelineWidth, timelineStart) {
+  return ((startTime - timelineStart) / timelineWidth) * 100 + "%";
+}
+
+function shoudBeVisible(startTime, duration, timelineStart, timelineWidth) {
+  const end = startTime + duration;
+  const timelineEnd = timelineStart + timelineWidth;
+  return (
+    (startTime >= timelineStart && startTime <= timelineEnd) ||
+    (end <= timelineEnd && end >= timelineStart)
   );
-}
-
-function getWidth(duration, timelineWidth){
-  return (duration/timelineWidth)*100+"%"
-}
-
-function getLeft(startTime, timelineWidth, timelineStart){
-  return (startTime-timelineStart)/timelineWidth*100+"%"
 }
