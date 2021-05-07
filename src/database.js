6c54027fc6986aeef68646e590c8e36f828f967f
jusola <juulain@gmail.com>
2021-05-07 20:19:18 +0300

    Add new schemas to db

--- src/database.js
+++ src/database.js
@@ -1,26 +1,31 @@
-import RxDB from 'rxdb'
+import RxDB from "rxdb";
+
+import eventSchema from "./schema/event.json";
+import trackSchema from "./schema/track.json";
+import timelineSchema from "./schema/timeline.json";
 
 export const initializeDB = async () => {
-    // create RxDB instance
-    const db = await RxDB.create({
-      name: 'slatedb',
-      adapter: 'idb',
-    });
-  
-    // add a collection to our db
-    await db.collection({
-      name: 'timelines',
-      schema: {
-        // ...
-      },
-    });
+  // create RxDB instance
+  const db = await RxDB.create({
+    name: "slatedb",
+    adapter: "idb",
+  });
 
-    await db.collection({
-        name: 'events',
-        schema: {
-          // ...
-        },
-      });
-  
-    return db;
+  // add a collection to our db
+  await db.collection({
+    name: "timelines",
+    schema: timelineSchema,
+  });
+
+  await db.collection({
+    name: "tracks",
+    schema: trackSchema,
+  });
+
+  await db.collection({
+    name: "events",
+    schema: eventSchema,
+  });
+
+  return db;
 };
