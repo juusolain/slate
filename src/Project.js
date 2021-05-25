03916aa4ac6b990207b472ee6d0670920a04df15
jusola <juulain@gmail.com>
2021-05-25 19:46:28 +0300

    Add modal util

--- src/Project.js
+++ src/Project.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 
 import { useRxCollection } from "rxdb-hooks";
 
-import Timeline from "./components/timeline/timeline/timeline";
+import Timeline from "./components/timeline/timeline";
 
 function Project() {
   const [currentTimeline, setCurrentTimeline] = useState();
--- src/components/util/modal.js
+++ src/components/util/modal.js
@@ -1,7 +1,7 @@
-function Modal(props) {
+export function Modal(props) {
   return (
     <div className="absolute translate-x-2/4 inset-0 w-screen h-screen">
-      <div className="mx-auto w-4/5">{props.children}</div>
+      <div className="m-auto w-4/5">{props.children}</div>
     </div>
   );
 }
