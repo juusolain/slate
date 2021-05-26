e7026825c1232b7cb05faf68d31cddc273da2807
jusola <juulain@gmail.com>
2021-05-26 11:59:23 +0300

    Modal improvements

--- src/Project.js
+++ src/Project.js
@@ -4,30 +4,62 @@ import { v4 as uuidv4 } from "uuid";
 import { useRxCollection } from "rxdb-hooks";
 
 import Timeline from "./components/timeline/timeline";
+import Modal from "./components/util/modal";
 
 function Project() {
   const [currentTimeline, setCurrentTimeline] = useState();
 
-  const timelines = useRxCollection("timelines");
+  const [modalType, setModalType] = useState(null);
 
-  console.log(timelines);
+  //const timelines = useRxCollection("timelines");
 
-  const createNewTimeline = () => {
+  /*const createNewTimeline = () => {
     const t = {
       id: uuidv4(),
       name: "Hi",
     };
     timelines.upsert(t);
     setCurrentTimeline(t.id);
+  };*/
+
+  const openModal = (newModal) => {
+    setModalType(newModal);
   };
+
+  const closeModal = () => {
+    setModalType(null);
+  };
+  function ModalContent() {
+    switch (modalType) {
+      case "timeline":
+        return <p>Timeline modal</p>;
+      case "track":
+        return <p>Track modal</p>;
+      default:
+        return null;
+    }
+  }
 
   return (
     <div className="w-screen">
-      <div className="bg-green-200 sticky flex p-1 items-start items-center m-2 rounded">
-        <button className="bg-green-500 rounded p-1 px-2 mx-1">Timeline</button>
-        {timelineModalOpen && <TimelineModal></TimelineModal>}
-        <button className="bg-green-500 rounded p-1 px-2 mx-1">Track</button>
-        {trackModalOpen && <TrackModal></TrackModal>}
+      {modalType && (
+        <Modal onClickOut={closeModal}>
+          <ModalContent />
+        </Modal>
+      )}
+      <div className="bg-green-300 sticky flex p-1 items-start items-center m-2 rounded">
+        <button
+          className="bg-green-700 text-gray-200 rounded p-1 px-2 mx-1"
+          onClick={() => openModal("timeline")}
+        >
+          Timeline
+        </button>
+        <button
+          className="bg-green-700 text-gray-200 rounded p-1 px-2 mx-1"
+          onClick={() => openModal("track")}
+        >
+          Track
+        </button>
       </div>
       <div>{currentTimeline && <Timeline id={currentTimeline}></Timeline>}</div>
     </div>
--- src/components/util/modal.js
+++ src/components/util/modal.js
@@ -1,7 +1,15 @@
-export function Modal(props) {
+export default function Modal({ onClickOut, ...props }) {
   return (
-    <div className="absolute translate-x-2/4 inset-0 w-screen h-screen">
-      <div className="m-auto w-4/5">{props.children}</div>
+    <div
+      className="flex justify-center bg-black bg-opacity-80 z-40 absolute translate-x-2/4 inset-0 w-screen h-screen"
+      onClick={(e) => {
+        e.preventDefault();
+        if (e.target === e.currentTarget) onClickOut();
+      }}
+    >
+      <div className="m-auto w-3/5 bg-green-200 z-50 h-3/5 p-2 rounded">
+        {props.children}
+      </div>
     </div>
   );
 }
